844fc6419a7ab743c8419c9a83cf715951e2260c
조성은 <jsesky1011@gmail.com>
2026-05-12 21:05:02 +0900

    chore: 캘린더 마이그레이션 호환 re-export 추가

---
+++ lib/core/network/calendar_day_cell.dart
@@ -0,0 +1,3 @@
+// This file is being migrated to features/calendar/presentation/widgets/calendar_day_cell.dart
+// Re-exporting for compatibility during transition
+export 'package:uyoung_app/features/calendar/presentation/widgets/calendar_day_cell.dart';
---
+++ lib/core/network/calendar_repository.dart
@@ -0,0 +1,3 @@
+// This file is being migrated to features/calendar/data/calendar_repository.dart
+// Re-exporting for compatibility during transition
+export 'package:uyoung_app/features/calendar/data/calendar_repository.dart';
